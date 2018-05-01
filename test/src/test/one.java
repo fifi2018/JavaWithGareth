0b3fc7c187c5a7757f55a0233ffec11b5c1106ef
fifi2018 <phyllis.gundu@qa.com>
2018-05-01 09:24:28 +0100

    test file

---
+++ test/src/test/one.java
@@ -0,0 +1,22 @@
+package test;
+
+import java.util.Random;
+
+public class one {
+
+	public static void main(String[] args) {
+		
+		int num = 10;
+		Random rand = new Random();
+		
+		for (int i = 1; i < 11; i++) {
+			System.out.println(i);
+			
+		
+		}
+		
+		System.out.println(rand.nextDouble());
+	}
+	
+
+}
