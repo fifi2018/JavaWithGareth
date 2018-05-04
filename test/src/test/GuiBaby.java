fb1ae1bea00fe92b7711b5ef1191a1352621e8ec
fifi2018 <phyllis.gundu@qa.com>
2018-05-04 16:12:23 +0100

    Last day Java

---
+++ test/src/test/GuiBaby.java
@@ -0,0 +1,59 @@
+package test;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import javax.swing.JTextPane;
+
+public class GuiBaby extends JFrame {
+
+	private JPanel contentPane;
+	private final JTextField textField = new JTextField();
+	private final JButton btnButton = new JButton("Button");
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GuiBaby frame = new GuiBaby();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GuiBaby() {
+		textField.setColumns(10);
+		initGUI();
+	}
+	public void textPrint() {
+		
+	}
+	private void initGUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		contentPane.add(textField, BorderLayout.NORTH);
+		
+		contentPane.add(btnButton, BorderLayout.EAST);
+	}
+
+}
